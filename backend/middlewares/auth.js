9b4a5f3b7ca8c13196d1573c9e250ec543cae59d
Dmitry <ds-8.5@bk.ru>
2023-09-04 17:14:45 +0300

    main. fix: исправил проверку на авторизацию пользователя, написал адреса в README.md

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-error');
 
@@ -9,7 +11,7 @@ module.exports = (req, res, next) => {
 
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (e) {
     next(new AuthError('Необходима авторизация'));
   }
